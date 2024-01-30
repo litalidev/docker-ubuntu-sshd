ea8bb893e6678b34bef123056c456ba90ba2ce55
Lita Li <9984614+litalidev@users.noreply.github.com>
2024-01-30 17:10:49 +0800

    new file:   build.sh - script to build docker image

---
+++ build.sh
@@ -0,0 +1,5 @@
+docker build --tag=litalidev/docker-ubuntu-sshd:22.04 .
+
+# docker image save litalidev/docker-ubuntu-sshd:22.04 > litalidev-docker-ubuntu-sshd-22.04.tar
+# docker import litalidev-docker-ubuntu-sshd-22.04.tar litalidev/docker-ubuntu-sshd:22.04
+# docker push litalidev/docker-ubuntu-sshd:22.04
